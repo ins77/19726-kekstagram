0c9acace8cb381ee93adcb2e8dda9870817f51f0
Dima Suhinin <suhinin.dm@gmail.ru>
2016-05-26 22:48:30 +0300

    module1-task2

---
+++ build/js/check.js
@@ -0,0 +1,39 @@
+var getMessage = function(a, b) {
+  var msg;
+
+  if (typeof(a) == "boolean") {
+
+    if (a) {
+      msg = "Переданное GIF-изображение анимировано и содержит " + b + " кадров";
+
+    } else {
+      msg = "Переданное GIF-изображение не анимировано";
+    }
+
+  } else if (typeof(a) == "number") {
+
+    msg = "Переданное SVG-изображение содержит " + a + " объектов и " + (b * 4) + " атрибутов";
+
+  } else if (Array.isArray(a)) {
+
+    var sum = 0;
+
+    for (var i = 0; i < a.length; i++) {
+      sum += a[i];
+    }
+
+    msg = "Количество красных точек во всех строчках изображения: " + sum;
+
+    if (Array.isArray(b)) {
+
+      var square = a.map(function(value, index) {
+        return value + b[index];
+      });
+
+      msg = "Общая площадь артефактов сжатия: " + square + " пикселей";
+    }
+
+  }
+
+  return msg;
+}
