ce74dcef17c1614f05746c282d358e48c375f922
Dima Suhinin <suhinin.dm@gmail.ru>
2016-05-28 23:43:49 +0300

    canvas

--- build/js/resizer.js
+++ build/js/resizer.js
@@ -120,6 +120,40 @@
           this._resizeConstraint.side - this._ctx.lineWidth / 2,
           this._resizeConstraint.side - this._ctx.lineWidth / 2);
 
+      // Отрисовка прямоугольника с дыркой внутри
+      this._ctx.beginPath();
+      this._ctx.fillStyle = 'rgba(0, 0, 0, 0.8)';
+      this._ctx.lineTo(this._container.width, -this._container.height / 2);
+      this._ctx.lineTo(this._container.width, this._container.height);
+      this._ctx.lineTo(-this._container.width / 2, this._container.height);
+      this._ctx.lineTo(
+        (-this._resizeConstraint.side / 2) - this._ctx.lineWidth,
+        (-this._resizeConstraint.side / 2) - this._ctx.lineWidth);
+      this._ctx.lineTo(
+        (this._resizeConstraint.side / 2) - this._ctx.lineWidth / 2,
+        (-this._resizeConstraint.side / 2) - this._ctx.lineWidth);
+      this._ctx.lineTo(
+        (this._resizeConstraint.side / 2) - this._ctx.lineWidth / 2,
+        (this._resizeConstraint.side / 2) - this._ctx.lineWidth / 2);
+      this._ctx.lineTo(
+        (-this._resizeConstraint.side / 2) - this._ctx.lineWidth,
+        (this._resizeConstraint.side / 2) - this._ctx.lineWidth / 2);
+      this._ctx.lineTo(
+        (-this._resizeConstraint.side / 2) - this._ctx.lineWidth,
+        (-this._resizeConstraint.side / 2) - this._ctx.lineWidth);
+      this._ctx.lineTo(-this._container.width / 2, this._container.height);
+      this._ctx.lineTo(-this._container.width / 2, -this._container.height / 2);
+      this._ctx.fill('evenodd');
+
+      // вывод текста с размерами изображения
+      this._ctx.fillStyle = '#fff';
+      this._ctx.font = '16px "Open Sans"';
+      this._ctx.textAlign = 'center';
+      this._ctx.fillText(
+        this._image.naturalWidth + ' x ' + this._image.naturalHeight,
+        -this._ctx.lineWidth,
+        (-this._resizeConstraint.side / 2) - this._ctx.lineWidth * 2);
+
       // Восстановление состояния канваса, которое было до вызова ctx.save
       // и последующего изменения системы координат. Нужно для того, чтобы
       // следующий кадр рисовался с привычной системой координат, где точка
